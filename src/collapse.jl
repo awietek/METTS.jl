2e21ba6430278671d79d7529d52c4da63fdaf186
Last-Hope-024 <rdscode95@gmail.com>
2026-05-27 18:53:27 +0200

    Fixed collpase.jl wrong ortogonality centers

--- src/collapse.jl
+++ src/collapse.jl
@@ -1,13 +1,12 @@
 function collapse(psi::MPS, basis::AbstractString=nothing)
     if basis === nothing || basis == "Z"
-        return sample(psi)
+        return sample!(psi)
     elseif basis == "X"
         s = siteinds(psi)
         N = length(psi)
         H_gates = [op("H", s[n]) for n in 1:N]
         psi2 = apply(H_gates, psi)
-        orthogonalize!(psi2, 1)
-        return sample(psi2)
+        return sample!(psi2)
     else
         error("Invalid basis specified in METTS collapse.")
     end
