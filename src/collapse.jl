9b3548d30075da50e3d31debbe16b62f5c5fa1d8
Last-Hope-024 <rdscode95@gmail.com>
2026-05-27 18:12:15 +0200

    Fixed basis rotation

--- src/collapse.jl
+++ src/collapse.jl
@@ -4,9 +4,8 @@ function collapse(psi::MPS, basis::AbstractString=nothing)
     elseif basis == "X"
         s = siteinds(psi)
         N = length(psi)
-        Ry_gates = [op("Ry", s[n], θ=π/2) for n in 1:N]
-        # Ry_gates = ops([("Ry", n, (θ=π / 2,)) for n in 1:N], s)
-        psi2 = apply(Ry_gates, psi)
+        H_gates = [op("H", s[n]) for n in 1:N]
+        psi2 = apply(H_gates, psi)
         orthogonalize!(psi2, 1)
         return sample(psi2)
     else
@@ -22,8 +21,8 @@ function collapse_with_qn(psi::MPS, basis::AbstractString=nothing)
         psi_noqn = dense(psi)
         s = siteinds(psi_noqn)
         N = length(psi_noqn)
-        Ry_gates = [op("Ry", s[n], θ=π/2) for n in 1:N]
-        psi_noqn = apply(Ry_gates, psi_noqn)
+        H_gates = [op("H", s[n]) for n in 1:N]
+        psi_noqn = apply(H_gates, psi_noqn)
         return sample!(psi_noqn)
     else
         error("Invalid basis specified in METTS collapse.")
